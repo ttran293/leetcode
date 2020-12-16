5f427097e9d97034289c34c4b5718c6d83a1c08b
ttran293 <45435344+ttran293@users.noreply.github.com>
2020-12-16 04:48:20 -0500

    Create code.py

---
+++ code.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range (len(nums)):
+            for j in range (len(nums)):
+                if i != j:
+                    if (nums[i] + nums[j]) == target:
+                        return i, j
