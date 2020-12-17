561a4fdc850feb05452db38cdc0976416e318db3
Thanh Nam Tran <45435344+ttran293@users.noreply.github.com>
2020-12-17 11:36:21 -0500

    Update 1-two-sum.py

--- 1-two-sum.py
+++ 1-two-sum.py
@@ -11,3 +11,20 @@ class Solution(object):
                 if i != j:
                     if (nums[i] + nums[j]) == target:
                         return i, j
+                    
+#hash table + enumerate solution by HankLiu5
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        h = {}
+        for i, num in enumerate(nums):
+            n = target - num
+            if n not in h:
+                h[num] = i
+            else:
+                return [h[n], i]
