b98be8062f4bca78e8a6be6d904927706864a8f3
Thanh Nam Tran <45435344+ttran293@users.noreply.github.com>
2020-12-28 14:52:55 -0500

    Create 222-count-complete-tree-nodes.py

---
+++ 222-count-complete-tree-nodes.py
@@ -0,0 +1,24 @@
+# Soluton found in discussion
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+        # @param {TreeNode} root
+        # @return {integer}
+        def countNodes(self, root):
+            if not root:
+                return 0
+            leftDepth = self.getDepth(root.left)
+            rightDepth = self.getDepth(root.right)
+            if leftDepth == rightDepth:
+                return pow(2, leftDepth) + self.countNodes(root.right)
+            else:
+                return pow(2, rightDepth) + self.countNodes(root.left)
+    
+        def getDepth(self, root):
+            if not root:
+                return 0
+            return 1 + self.getDepth(root.left)
